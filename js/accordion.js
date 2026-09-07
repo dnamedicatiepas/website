1ac0756a6a4d3867e46edbd33fe4196494178c74
Daniel Tran <danieltran354@gmail.com>
2026-09-07 14:05:10 +0200

    First version: Add FAQ section with accordion functionality and adjust layout styles

---
+++ js/accordion.js
@@ -0,0 +1,24 @@
+window.addEventListener('load', init);
+
+let acc;
+let i;
+
+function init() {
+    acc = document.getElementsByClassName("accordion");
+    i;
+
+    for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var panel = this.nextElementSibling;
+            if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+            } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+            }
+        });
+}
+}
+
+
+
